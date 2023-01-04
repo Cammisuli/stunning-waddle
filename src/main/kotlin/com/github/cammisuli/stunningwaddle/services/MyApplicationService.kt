2f03f78906b4863044af6f9b211ef935509d010a
GitHub Action <action@github.com>
2023-01-04 19:34:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/cammisuli/stunningwaddle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cammisuli/stunningwaddle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cammisuli.stunningwaddle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cammisuli.stunningwaddle.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cammisuli/stunningwaddle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cammisuli/stunningwaddle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cammisuli.stunningwaddle.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cammisuli.stunningwaddle.MyBundle
 
 class MyProjectService(project: Project) {
 
